bb48a7808d0d37f94edbe05885b67e0a5bf1bdf7
den-m1985 <denismartyanov244@gmail.com>
2023-03-26 08:03:06 +0200

    remove project

---
+++ src/main/java/org/example/Main.java
@@ -0,0 +1,15 @@
+package org.example;
+
+import org.example.window.Window;
+
+import java.io.IOException;
+
+public class Main {
+
+    public static void main(String[] args) throws IOException {
+
+        Window window = new Window();
+        window.setVisible(true);
+
+    }
+}
