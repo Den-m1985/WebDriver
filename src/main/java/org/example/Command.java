5308aa701a5dedde63efa6202c74ad552ebadbed
den-m1985 <denismartyanov244@gmail.com>
2023-04-11 12:43:54 +0200

    move to search and add

--- src/main/java/org/example/Command.java
+++ src/main/java/org/example/Command.java
@@ -5,6 +5,7 @@ import org.example.browser.*;
 import org.example.browser.chrome.OpenChrome;
 import org.example.csvRead.CsvFilter;
 import org.example.oldExel.WrightOldExelArticul;
+import org.example.searchAndAdd.SearchAndAdd;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
@@ -54,8 +55,6 @@ public class Command {
             System.out.println(x.getText());
         }
 
-        AddGoods addGoods = new AddGoods(wait);
-        SearchGoods searchGoods = new SearchGoods(wait);
 
         for (String[] goods : data) {
             String goodsName = goods[0];
@@ -63,65 +62,13 @@ public class Command {
             int intGoodsPrice = Integer.parseInt(goods[2]);
             String goodsItem = goods[3];
 
-            //если всплывает окно, то закрываем.
-//            WebElement cloudWindow = driver.findElement(By.id("dontgo"));
-//            System.out.println(cloudWindow.isDisplayed());
-//            if (cloudWindow.isDisplayed()) {
-//                TextLinks closeWindow = TextLinks.CLOSEWINDOW;
-//                cloudWindow.findElement(By.xpath(closeWindow.getString())).click();
-//            }
-
-            new ClowdWindow(driver);
-
-            // Search goods
-            searchGoods.searchProduct(goodsName, driver);
-
-            new ClowdWindow(driver);
-
-            // находим несколько имен в поисковике
-            List<WebElement> products = driver.findElements(By.className("products"));
-
-            // проверяем на наличие товара
-            List<WebElement> product = driver.findElements(By.className("product"));
-
-            // проверяем на ниличие выбора размера
-            List<WebElement> size = driver.findElements(By.className("b1c_option"));
-
-            // если товара в поисковике более 1шт
-            if (products.size() > 0) {
-                //System.out.println("Товаров более 1шт." + goodsName);
-                String[] noFind = {goodsName, "товаров более 1шт."};
-                reportList.add(noFind);
-
-                // если товар есть
-            } else if (product.size() > 0) {
-
-                // если надо выбрать размер
-                if (size.get(0).getText().length() > 0) {
-
-                    CommandSelectSize selectSize = new CommandSelectSize(driver, addGoods);
-                    selectSize.commandSelectSize(goodsName, goodsSize, intGoodsPrice, goodsItem);
-                    new ClowdWindow(driver);
-
-                    if (selectSize.getReportList() != null) {
-                        reportList.add(selectSize.getReportList());
-                        //System.out.println(selectSize.getReportList().length);
-                        //System.out.println(selectSize.getReportList()[0]);
-                        //System.out.println(selectSize.getReportList()[1]);
-                    }
-
-                } else {
-                    //System.out.println(goodsName);
-                    CheckPrice check = new CheckPrice(driver, intGoodsPrice);
-                    if (check.checkPrice()) {
-                        addGoods.addGoods(goodsItem, driver);  // товар найден, добавляем в корзину
-                        new ClowdWindow(driver);
-                    }
-                }
-
-            } else {
-                String[] noFind = {goodsName, "товар НЕ найден"};
-                reportList.add(noFind);
+            try {
+                SearchAndAdd searchAndAdd = new SearchAndAdd(driver, wait, reportList);
+                searchAndAdd.executeProcess(goodsName, goodsSize, intGoodsPrice, goodsItem);
+            } catch (Exception e) {
+                System.out.println("Произошла ошибка: " + e.getMessage());
+                String[] noAdd = {goodsName, "Какая-то общая ошибка"};
+                reportList.add(noAdd);
             }
         }
 
