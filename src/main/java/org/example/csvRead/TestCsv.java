bed39d8b71b344cdacfa82a2bb7d68b0d9cf9382
den-m1985 <denismartyanov244@gmail.com>
2023-04-12 16:42:30 +0200

    test

--- src/main/java/org/example/csvRead/TestCsv.java
+++ src/main/java/org/example/csvRead/TestCsv.java
@@ -5,69 +5,53 @@ import org.example.TextLinks;
 
 import java.io.IOException;
 import java.util.ArrayList;
+import java.util.Collections;
+import java.util.Comparator;
 import java.util.List;
 
 public class TestCsv {
+
+    private static List<StructureCSV> dublicatNames;
+
     public static void main(String[] args) throws IOException, CsvException {
         int cellName = 0;   // Cell with name or articular
         int cellPrice = 2;
         int cellItem = 3;
 
 
-        CsvRead csvRead = new CsvRead("C:\\Users\\Den\\Downloads\\альфа 11.04.csv");
+        CsvRead csvRead = new CsvRead("C:\\Users\\Den\\Downloads\\альфа 11.04 ТЕСТ.csv");
         List<String[]> rows = csvRead.readCSV();
 
-        // В этом блоке отсеиваваем лишнее
-        List<String[]> dataWithItem = new ArrayList<>();
+        // В этом блоке оставляем только те колонки где есть цена и кол-во
+
         // check - if cell item is integer?
-        for (String[] row : rows) {
-            if (isInteger(row[cellItem]) && isInteger(row[cellPrice]))
-                dataWithItem.add(row);
-        }
+        List<StructureCSV> dataWithItem = metod1(rows, cellPrice, cellItem);
+
+
+        int rtyghsb = 1000000;
+        long start = System.nanoTime();
+
 
         // этот блок возвращает иникальные элементы
-        List<String[]> uniqueValues = new ArrayList<>();
-        List<String[]> dublicatNames = new ArrayList<>();
-        int i = 0;
-        for (String[] row : dataWithItem) {
-            String name = row[cellName];
-            for (String[] value : dataWithItem) {
-                String name2 = value[cellName];
-                if (name.equals(name2)) {
-                    i++;
-                    if (i > 1) {
-                        dublicatNames.add(row);
-                        break;
-                    }
-                }
-            }
-            if (i == 1) {
-                uniqueValues.add(row);
-            }
-            i = 0;
-        }
+
+        List<StructureCSV> uniqueValues = metod2(dataWithItem);
 
         // этот блок работатет с повторяющимися именами.
-        int j = 0;
-        for (String[] row1 : dublicatNames) {
-            String name = row1[cellName];
-            String price = row1[cellPrice];
-            String item = row1[cellItem];
-            for (String[] row2 : dublicatNames){
-                String name2 = row2[cellName];
-                String price2 = row2[cellPrice];
-                String item2 = row2[cellItem];
-                if (name.equals(name2)) {
-                    j++;
-                    if (j > 1) {
-                        //dublicatNames.add(row);
-                        break;
-                    }
-            }
+        List<StructureCSV> dublicatNames2 = new ArrayList<>();
+        dublicatNames2 = metod3();
+        uniqueValues.addAll(dublicatNames2);
 
-            }
+        for (StructureCSV x : uniqueValues) {
+            System.out.println(x.getName() + "---" + x.getArtucul() + "---" + x.getItem());
         }
 
+
+        long end = System.nanoTime();
+        long a = end - start;
+        long time = a / rtyghsb;
+        System.out.println();
+        System.out.println("Время выполнения: " + time);
+        System.out.println();
 /*
         java.
         есть: List<String[]> dublicatNames = new ArrayList<>();
@@ -83,14 +67,24 @@ public class TestCsv {
         если name повторяется, а size нет, то добавляем в Names.
 */
 
-        TextLinks textLinks = TextLinks.COUNROWSCSV;
-        System.out.println();
-        System.out.println(textLinks.getString() + uniqueValues.size());
-        System.out.println(dublicatNames.get(0));
 
-//        for (String [] x:dublicatNames) {
-//            System.out.println(x[0]);
+
+        /*
+        Вариант 2
+         */
+//        long start2 = System.nanoTime();
+//
+//        Collections.sort(dublicatNames, Comparator.comparing(arr -> arr[0]));
+//
+//        for (String[] x : dublicatNames) {
+//            System.out.println(x[0] + "---" + x[1] + "---" + x[3]);
 //        }
+//
+//        long end2 = System.nanoTime();
+//        long a2 = end2 - start2;
+//        long time2 = a2 / rtyghsb;
+//        System.out.println();
+//        System.out.println("Время выполнения: " + time2);
 
     }
 
@@ -103,4 +97,79 @@ public class TestCsv {
             return false;
         }
     }
+
+
+    public static List<StructureCSV> metod1(List<String[]> rows, int cellPrice, int cellItem) {
+
+
+        List<StructureCSV> dataWithItem = new ArrayList<>();
+        for (String[] row : rows) {
+            if (isInteger(row[cellItem]) && isInteger(row[cellPrice])) {
+                int price = Integer.parseInt(row[cellPrice]);
+                int item = Integer.parseInt(row[cellItem]);
+
+                dataWithItem.add(new StructureCSV(row[0], row[1], price, item));
+                //dataWithItem.add(row);
+            }
+        }
+        return dataWithItem;
+    }
+
+
+    public static List<StructureCSV> metod2(List<StructureCSV> dataWithItem) {
+
+        List<StructureCSV> uniqueValues = new ArrayList<>();
+        dublicatNames = new ArrayList<>();
+        int i = 0;
+        for (StructureCSV row : dataWithItem) {
+            String name = row.getName();
+            for (StructureCSV value : dataWithItem) {
+                String name2 = value.getName();
+                if (name.equals(name2)) {
+                    i++;
+                    if (i > 1) {
+                        dublicatNames.add(row);
+                        break;
+                    }
+                }
+            }
+            if (i == 1) {
+                uniqueValues.add(row);
+            }
+            i = 0;
+        }
+        return uniqueValues;
+    }
+
+
+    public static List<StructureCSV> metod3() {
+        List<StructureCSV> Names = new ArrayList<>();
+        for (StructureCSV row : dublicatNames) {
+            String name = row.getName();
+            String size = row.getArtucul();
+            int price = row.getPrice();
+            int item = row.getItem();
+            boolean duplicated = false;
+            for (StructureCSV existing : Names) {
+                String str1 = existing.getName();
+                String str2 = existing.getArtucul();
+                int existingItem = existing.getItem();
+                if (str1.equals(name)) {
+                    if (str2.equals(size)) {
+                        existing.setItem(existingItem + item);
+                        duplicated = true;
+                        break;
+                    } else {
+                        continue;
+                    }
+                }
+            }
+            if (!duplicated) {
+                Names.add(new StructureCSV (name, size, price, item));
+            }
+        }
+        return Names;
+    }
+
+
 }
