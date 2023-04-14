db3efbce62f5abb4c68a9703d867332c7695ecd3
den-m1985 <denismartyanov244@gmail.com>
2023-04-14 10:51:41 +0200

    14.04.2023 refactor String[] to StructureCSV

--- src/main/java/org/example/csvRead/csv/UniqueGoods.java
+++ src/main/java/org/example/csvRead/csv/UniqueGoods.java
@@ -4,20 +4,22 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class UniqueGoods {
-    private List<String[]> duplicateNames;
+    private List<StructureCSV> duplicateNames;
 
     public UniqueGoods() {
     }
 
-    public List<String[]> uniqueGoods(List<String[]> dataWithItem, int cellName) {
+    public List<StructureCSV> uniqueGoods(List<StructureCSV> dataWithItem) {
 
-        List<String[]> uniqueValues = new ArrayList<>();
+
+
+        List<StructureCSV> uniqueValues = new ArrayList<>();
         duplicateNames = new ArrayList<>();
         int i = 0;
-        for (String[] row : dataWithItem) {
-            String name = row[cellName];
-            for (String[] value : dataWithItem) {
-                String name2 = value[cellName];
+        for (StructureCSV row : dataWithItem) {
+            String name = row.getName();
+            for (StructureCSV value : dataWithItem) {
+                String name2 = value.getName();
                 if (name.equals(name2)) {
                     i++;
                     if (i > 1) {
@@ -31,10 +33,33 @@ public class UniqueGoods {
             }
             i = 0;
         }
+
+
+
+//        List<String[]> uniqueValues = new ArrayList<>();
+//        duplicateNames = new ArrayList<>();
+//        int i = 0;
+//        for (String[] row : dataWithItem) {
+//            String name = row[cellName];
+//            for (String[] value : dataWithItem) {
+//                String name2 = value[cellName];
+//                if (name.equals(name2)) {
+//                    i++;
+//                    if (i > 1) {
+//                        duplicateNames.add(row);
+//                        break;
+//                    }
+//                }
+//            }
+//            if (i == 1) {
+//                uniqueValues.add(row);
+//            }
+//            i = 0;
+//        }
         return uniqueValues;
     }
 
-    public List<String[]> getDuplicateNames() {
+    public List<StructureCSV> getDuplicateNames() {
         return duplicateNames;
     }
 }
