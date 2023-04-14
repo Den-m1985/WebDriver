4812ce088f04f3306c45934c3aee53b52c7d9606
den-m1985 <denismartyanov244@gmail.com>
2023-04-14 09:36:35 +0200

    14.04.2023 refactor code using classes

--- src/main/java/org/example/csvRead/CsvFilter.java
+++ src/main/java/org/example/csvRead/CsvFilter.java
@@ -2,118 +2,51 @@ package org.example.csvRead;
 
 import com.opencsv.exceptions.CsvException;
 import org.example.TextLinks;
+import org.example.csvRead.csv.CsvRead;
+import org.example.csvRead.csv.DuplicateGoods;
+import org.example.csvRead.csv.OnlyGoods;
+import org.example.csvRead.csv.UniqueGoods;
 
 import java.io.IOException;
-import java.util.ArrayList;
 import java.util.List;
 
 public class CsvFilter {
     private final String fileName;
-    private final List<String[]> reportList;
-    private List<String[]> dublicatNames;
 
 
-    public CsvFilter(String fileName, List<String[]> reportList) {
+    public CsvFilter(String fileName) {
         this.fileName = fileName;
-        this.reportList = reportList;
     }
 
-    public List<String[]> csvFilter(int cellName,int cellPrice, int cellItem) throws IOException, CsvException {
+    public List<String[]> csvFilter(int cellName, int cellPrice, int cellItem) throws IOException, CsvException {
 
         CsvRead csvRead = new CsvRead(fileName);
         List<String[]> rows = csvRead.readCSV();
 
         // В этом блоке оставляем только те колонки где есть цена и кол-во
-        List<String[]> dataWithItem = OnlyGoods(rows, cellPrice, cellItem);
+        List<String[]> dataWithItem = new OnlyGoods().onlyGoods(rows, cellPrice, cellItem);
 
         // этот блок возвращает иникальные элементы
-        List<String[]> uniqueValues = uniqGoods(dataWithItem, cellName);
+        UniqueGoods uniqueGoods = new UniqueGoods();
+        List<String[]> uniqueValues = uniqueGoods.uniqueGoods(dataWithItem, cellName);
+        List<String[]> duplicateNames = uniqueGoods.getDuplicateNames();
 
         // этот блок работатет с повторяющимися именами.
-        List<String[]> resolveDublicatedNames = dublicateGoods();
-        uniqueValues.addAll(resolveDublicatedNames);
+        List<String[]> resolveDuplicatedNames = new DuplicateGoods().duplicateGoods(duplicateNames);
+        uniqueValues.addAll(resolveDuplicatedNames);
 
         TextLinks textLinks = TextLinks.COUNROWSCSV;
         System.out.println();
         System.out.println(textLinks.getString() + uniqueValues.size());
 
+//        for (String[]x:duplicateNames) {
+//            System.out.println(x[0] + "----" + x[1] + "----" + x[3]);
+//        }
+//        System.out.println();
+//        for (String[]x:resolveDuplicatedNames) {
+//            System.out.println(x[0] + "----" + x[1] + "----" + x[3]);
+//        }
         return uniqueValues;
-    }
-
-
-    static boolean isInteger(String str) {
-        try {
-            Integer.parseInt(str);
-            return true;
-        } catch (NumberFormatException e) {
-            return false;
-        }
-    }
-
-    public static List<String[]> OnlyGoods(List<String[]> rows, int cellPrice, int cellItem) {
-
-        List<String[]> dataWithItem = new ArrayList<>();
-        for (String[] row : rows) {
-            if (isInteger(row[cellItem]) && isInteger(row[cellPrice]))
-                dataWithItem.add(row);
-        }
-        return dataWithItem;
-    }
-
-
-    public List<String[]> uniqGoods(List<String[]> dataWithItem, int cellName) {
-
-        List<String[]> uniqueValues = new ArrayList<>();
-        dublicatNames = new ArrayList<>();
-        int i = 0;
-        for (String[] row : dataWithItem) {
-            String name = row[cellName];
-            for (String[] value : dataWithItem) {
-                String name2 = value[cellName];
-                if (name.equals(name2)) {
-                    i++;
-                    if (i > 1) {
-                        dublicatNames.add(row);
-                        break;
-                    }
-                }
-            }
-            if (i == 1) {
-                uniqueValues.add(row);
-            }
-            i = 0;
-        }
-        return uniqueValues;
-    }
-
-
-    public List<String[]> dublicateGoods() {
-        List<String[]> Names = new ArrayList<>();
-        for (String[] row : dublicatNames) {
-            String name = row[0];
-            String size = row[1];
-            String price = row[2];
-            String item = row[3];
-            boolean duplicated = false;
-            for (String[] existing : Names) {
-                String str1 = existing[0];
-                String str2 = existing[1];
-
-                if (str1.equals(name)) {
-                    if (str2.equals(size)) {
-                        existing[3] = String.valueOf(Integer.parseInt(existing[3]) + Integer.parseInt(item));
-                        duplicated = true;
-                        break;
-                    } else {
-                        continue;
-                    }
-                }
-            }
-            if (!duplicated) {
-                Names.add(new String[]{name, size, price, item});
-            }
-        }
-        return Names;
     }
 
 
